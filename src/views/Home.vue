8a47e43bc8ff3487f2cf40818a6936590d41a5da
aoirint <aoirint@gmail.com>
2022-06-15 02:36:41 +0900

    init lastEngineState with STARTING

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -517,16 +517,18 @@ export default defineComponent({
     // TODO: 個別のエンジンの状態をUIで確認できるようにする
     const allEngineState = computed(() => {
       const engineStates = store.state.engineStates;
+      const engineKeys = store.state.engineKeys;
 
-      let lastEngineState: EngineState | undefined = undefined;
+      // NOTE: 1つもエンジンが登録されていない場合はSTARTING
+      let lastEngineState: EngineState = "STARTING";
 
       // 登録されているすべてのエンジンについて状態を確認する
-      for (const engineKey of store.state.engineKeys) {
+      for (const engineKey of engineKeys) {
         const engineState: EngineState | undefined = engineStates[engineKey];
         if (engineState === undefined)
           throw new Error(`No such engineState set: engineKey == ${engineKey}`);
 
-        // FIXME: 1つでも接続テストに成功していないエンジンがあれば、暫定的に起動中とする
+        // NOTE: 1つでも接続テストに成功していないエンジンがあれば、暫定的に起動中とする
         if (engineState === "STARTING") {
           return engineState;
         }
@@ -534,7 +536,7 @@ export default defineComponent({
         lastEngineState = engineState;
       }
 
-      return lastEngineState; // FIXME: 暫定的に1つのエンジンの状態を返す
+      return lastEngineState; // NOTE: 暫定的に1つのエンジンの状態を返す
     });
 
     // ライセンス表示
